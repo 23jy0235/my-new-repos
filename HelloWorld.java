e4eb7a85e01fece0528a223824a0a6bf97ae982d
23jy0235 <23jy0235@jec.ac.jp>
2024-10-16 12:02:19 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
-import java.util.Random;
-
 public class HelloWorld{
   public static void main(String[] args) {
-		Random r=new Random();
-		String[] word= {"Hello","HelloWorld"};
-		for(int i=0;i<=12;i++) {
-			String rs=word[r.nextInt(0,2)];
-			System.out.println(rs);
-		}
+	System.out.println("HelloWorld");
+	System.out.println("HelloWorld");
+	System.out.println("HelloWorld");
 }
